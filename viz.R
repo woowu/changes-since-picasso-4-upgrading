ff1a66e8dbb2c329df1a869db4d7caebb06cd541
Woody Wu <woody.wu2@landisgyr.com>
2022-12-05 11:33:17 +0800

    update plot

--- viz.R
+++ viz.R
@@ -3,13 +3,18 @@ library(tidyverse)
 library(ggplot2)
 
 data <- read.csv('changes-functional-summary.csv')
-data <- data %>% mutate(Changes = Plus + Minus)
-head(data)
 
-p <- ggplot(data, aes(x=Changes, y=Group)) +
+p <- ggplot(data, aes(x=Changes, y=Module)) +
     geom_point(aes(color=Layer, shape=Todo)) +
-    theme(axis.text=element_text(size=5)) +
-    #theme(legend.position='none') +
+    theme(axis.text=element_text(size=5), legend.text=element_text(size=6), legend.title=element_text(size=8)) +
     labs(y='Function', x='Changed Lines')
 
 ggsave('changes-functional-summary.png', p, dpi=300)
+
+p <- ggplot(data, aes(x=Score, y=Module)) +
+    geom_point(aes(color=Layer, shape=Todo)) +
+    theme(axis.text=element_text(size=5), legend.text=element_text(size=6), legend.title=element_text(size=8)) +
+    labs(y='Function', x='Score')
+
+ggsave('changes-functional-rating.png', p, dpi=300)
+
